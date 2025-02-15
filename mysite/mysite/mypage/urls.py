4568280dc1906c9528c383d4c2b3ec6cfe4dc7cc
Anoop Krishnan Ramachandran <anoop4u777@gmail.com>
2025-02-15 14:32:19 -0500

    My site

--- mysite/mysite/mypage/urls.py
+++ mysite/mysite/mypage/urls.py
@@ -3,5 +3,5 @@ from .views import index
 
 
 urlpatterns = [
-    path('helloworld/', index),
+    path('', index, name="index"),
 ]
--- mysite/mysite/mysite/urls.py
+++ mysite/mysite/mysite/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from mypage.urls import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("/", my)
+    path("", include("mypage.urls"))
 ]
